79ce370b4bdf5347819b77851f843a0de7b9d0a8
LSeyes <105695041+LSeyes@users.noreply.github.com>
2023-06-08 16:16:02 +0800

    Create jiaoben.py
    update

---
+++ jiaoben.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+wd = webdriver.Chrome()
+wd.implicitly_wait(10)
+import time
+from selenium.webdriver.support.ui import Select
+
+url = "http://222.171.146.55/"
+
+wd.get(url)
+
+id = wd.find_element(By.CSS_SELECTOR,'#input_username')
+#id.send_keys('你的学号')
+id.send_keys('')
+#passwd.send_keys('你的密码')
+passwd = wd.find_element(By.CSS_SELECTOR,'#input_password')
+passwd.send_keys('')
+
+
+
+#请自己输一下验证码，输完验证码不要点登录
+# 输完验证码不要点登录
+# 输完验证码不要点登录
+# 输完验证码不要点登录
+# 输完验证码不要点登录
+
+time.sleep(8)
+#自己输入验证码
+
+loginbutton = wd.find_element(By.CSS_SELECTOR,'#loginButton')
+loginbutton.click()
+
+
+#教学评估目录按钮
+
+menulist = wd.find_element(By.CSS_SELECTOR,'#menu-toggler')
+menulist.click()
+time.sleep(2)
+jiaoxuelist = wd.find_element(By.XPATH,'//*[@id="12580300"]')
+jiaoxuelist.click()
+jiaoxuepinggu = wd.find_element(By.XPATH,'//*[@id="12580302"]')
+jiaoxuepinggu.click()
+
+#待评估课程
+no_pinggu_list = wd.find_elements(By.CSS_SELECTOR,'tbody#jxpgtbody tr')
+
+for i in range(len(no_pinggu_list)):
+    no_pinggu_list = wd.find_elements(By.CSS_SELECTOR, 'tbody#jxpgtbody tr')
+    item = no_pinggu_list[i]
+    x = item.find_element(By.CSS_SELECTOR,'button').text
+    print(x)
+
+    if x == '查看':
+        continue
+
+    pingu_button = item.find_element(By.CSS_SELECTOR,'td')
+    pingu_button.click()
+
+    time.sleep(2)
+    buttonlist = wd.find_elements(By.CSS_SELECTOR,'tbody tr')
+    buttonlist[2].find_element(By.CSS_SELECTOR, 'div span').click()
+    buttonlist[4].find_element(By.CSS_SELECTOR, 'div span').click()
+    buttonlist[6].find_element(By.CSS_SELECTOR, 'div span').click()
+    buttonlist[8].find_element(By.CSS_SELECTOR, 'div span').click()
+    time.sleep(1)
+    wd.find_element(By.CSS_SELECTOR,'textarea').send_keys('无')
+
+    #按提交
+    wd.find_element(By.CSS_SELECTOR,'#buttonSubmit').click()
+
+    wd.find_element(By.CSS_SELECTOR,'div.layui-layer-btn a').click()
